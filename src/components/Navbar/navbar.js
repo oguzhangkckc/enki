155a13468bd575aae9a948525a552b569a8083f5
Oğuzhan Gökcekoca <ogokcekoca@gmail.com>
2023-06-03 23:00:45 +0300

    eklemeler

--- src/components/Navbar/navbar.js
+++ src/components/Navbar/navbar.js
@@ -1,7 +1,20 @@
-import React from 'react'
+import React from 'react';
 
-export default function Navbar() {
+const Navbar = () => {
   return (
-    <div>navbar</div>
-  )
-}
+      <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+        <div></div>
+        <div className="">
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Industries</li>
+          <li>Contact</li>
+        </div>
+        <div></div>
+
+      </nav>
+  );
+};
+
+
+export default Navbar;
