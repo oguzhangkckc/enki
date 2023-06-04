ff434b9d59331c38defbd0880cb3e61e58865fa9
Oğuzhan Gökcekoca <ogokcekoca@gmail.com>
2023-06-04 13:09:25 +0300

    foti eklemeleri

--- src/components/Navbar/navbar.js
+++ src/components/Navbar/navbar.js
@@ -7,12 +7,16 @@ const Navbar = () => {
     links.forEach((link) => {
       link.addEventListener('mouseenter', function () {
         const underline = this.querySelector('.underline');
-        underline.style.width = '100%';
+        if (underline) {
+          underline.style.width = '100%';
+        }
       });
 
       link.addEventListener('mouseleave', function () {
         const underline = this.querySelector('.underline');
-        underline.style.width = '0%';
+        if (underline) {
+          underline.style.width = '0%';
+        }
       });
     });
   }, []);
@@ -22,11 +26,11 @@ const Navbar = () => {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-20">
           <div className="flex-shrink-0">
-            <h1 className="text-red-500 text-4xl sm:text-5xl lg:text-6xl font-extrabold">enki</h1>
+            <h1 className="text-red-500 text-6xl font-extrabold">enki</h1>
           </div>
 
           <div className="">
-            <ul className="flex items-center space-x-2 sm:space-x-4 lg:space-x-8 text-base sm:text-lg lg:text-xl">
+            <ul className="flex items-center font-bold space-x-8 text-base text-xl hidden md:flex">
               <li className="relative">
                 <a href="/" className="text-black hover:text-red-500 transition duration-500 hover-underline-left-to-right">
                   Home
@@ -55,9 +59,7 @@ const Navbar = () => {
           </div>
 
           <div>
-            <button className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow">
-              Login
-            </button>
+
           </div>
         </div>
       </div>
--- src/components/pages/Home/home.js
+++ src/components/pages/Home/home.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 export default function Home() {
   const imageUrls = [
@@ -10,16 +10,130 @@ export default function Home() {
     "https://www.enkiticaret.com.tr/1/images/-2aaadbc6527c41559078363f7995d78f.png",
   ];
 
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+
+  const goToPreviousSlide = () => {
+    setCurrentImageIndex((prevIndex) => (prevIndex === 0 ? imageUrls.length - 1 : prevIndex - 1));
+  };
+
+  const goToNextSlide = () => {
+    setCurrentImageIndex((prevIndex) => (prevIndex + 1) % imageUrls.length);
+  };
+
+  useEffect(() => {
+    const storedIndex = localStorage.getItem('currentImageIndex');
+    if (storedIndex) {
+      setCurrentImageIndex(parseInt(storedIndex));
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('currentImageIndex', currentImageIndex.toString());
+  }, [currentImageIndex]);
+
   return (
-    <div className="bg-white w-screen h-1/2 flex overflow-x-auto">
-      {imageUrls.map((imageUrl, index) => (
+    <div className="bg-white w-screen h-1/2 relative">
+      <div>
+        <div className="relative">
+          <div className="flex w-full h-full overflow-x-auto">
+            {imageUrls.map((imageUrl, index) => (
+              <div key={index} className={`flex-shrink-0 w-screen ${index === currentImageIndex ? 'block' : 'hidden'}`}>
+                <img
+                  src={imageUrl}
+                  alt=""
+                  className="h-full w-full object-cover"
+                />
+              </div>
+            ))}
+          </div>
+          <button
+            className="absolute top-1/2 left-4 transform -translate-y-1/2 bg-gray-200 hover:bg-gray-300 text-gray-800 font-bold py-2 px-4 rounded-full"
+            onClick={goToPreviousSlide}
+          >
+            ←
+          </button>
+          <button
+            className="absolute top-1/2 right-4 transform -translate-y-1/2 bg-gray-200 hover:bg-gray-300 text-gray-800 font-bold py-2 px-4 rounded-full"
+            onClick={goToNextSlide}
+          >
+            →
+          </button>
+        </div>
+
+        <div className="absolute top-60 left-1/2 transform -translate-x-1/2 -translate-y-1/2 hidden lg:flex">
+          <div className="bg-white bg-opacity-40 p-8 w-84 h-80 flex flex-col shadow-lg shadow-red-500 justify-center rounded-3xl items-center">
+            <p className="text-black font-bold text-2xl text-center">
+              "We offer solutions that provide a cost advantage with our expert and experienced personnel who have been working in the sector for a long time"
+            </p>
+            <button className="shadow hover:shadow-2xl mt-4 px-6 py-2 rounded-full bg-red-500 text-white hover:bg-gray-700 transition duration-300">
+              <a href="/industries">Industries →</a>
+            </button>
+          </div>
+        </div>
+      </div>
+      <div className='flex items-row gap-x-5 my-14 mx-16'>
+        <div>
+          <img src="https://www.enkiticaret.com.tr/1/img/enri1.png" alt="" className="hover:grayscale hover:-translate-y-1 transition duration-500"
+          />
+        </div>
+
+        <div>
+          <img src="https://www.enkiticaret.com.tr/1/img/kcc1.png" alt="" className="hover:grayscale hover:-translate-y-1 transition duration-500"
+          />
+        </div>
+
+        <div>
+          <img src="https://www.enkiticaret.com.tr/1/img/midgold1.png" alt="" className="hover:grayscale hover:-translate-y-1 transition duration-500" />
+        </div>
+        <div>
+          <img src="https://www.enkiticaret.com.tr/1/img/pandish1.png" alt="" className="hover:grayscale hover:-translate-y-1 transition duration-500" />
+        </div>
+        <div>
+          <img src="https://www.enkiticaret.com.tr/1/img/polyguar1.png" alt="" className="hover:grayscale hover:-translate-y-1 transition duration-500" />
+        </div>
+        <div>
+          <img src="https://www.enkiticaret.com.tr/1/img/edda1.png" alt="" className="hover:grayscale hover:-translate-y-1 transition duration-500" />
+        </div>
+        <div>
+          <img src="https://www.enkiticaret.com.tr/1/img/mph1.png" alt="" className="hover:grayscale hover:-translate-y-1 transition duration-500" />
+        </div>
+      </div>
+      <div className="relative">
         <img
-          key={index}
-          src={imageUrl}
+          src="https://www.teqfocus.com/wp-content/uploads/2019/02/home-infotechno-video-intro-bg.jpg"
           alt=""
-          className="h-full min-w-full object-cover"
+          className="w-full h-1/2 object-cover"
         />
-      ))}
+        <h1 className="absolute top-1/2 right-20 transform -translate-y-1/2 text-4xl font-bold text-white">
+          We offer solutions with our expert<br /> and experienced staff.
+        </h1>
+      </div>
+      <div>
+        <h2 className='text-sm text-gray-400 font-bold text-center my-10'>I N D U S T R I E S</h2>
+        <h1 className='text-2xl font-bold text-center my-10'>Our Activity <span className='text-red-500'>Areas</span></h1>
+        <div className='flex items-row justify-center gap-x-10 my-14 mx-16'>
+          <div>
+            <img src='https://www.enkiticaret.com.tr/1/images/-c0147a394e1246ada5c8b83bf10e2e70.jfif' alt='' className='w-80 h-80 hover:-translate-y-1 transition duration-500 rounded-t-2xl' />
+          </div>
+          <div>
+            <img src='https://www.enkiticaret.com.tr/1/images/-5255e46a1e0a4ad881e9bdf817dd4b05.jfif' alt="" className='w-80 h-80 hover:-translate-y-1 transition duration-500 rounded-t-2xl' />
+          </div>
+          <div>
+            <img src='https://www.enkiticaret.com.tr/1/images/-0d806bb361234af4b2aed48e13a581db.jfif' alt='' className='w-80 h-80 hover:-translate-y-1 transition duration-500 rounded-t-2xl' />
+          </div>
+        </div>
+        <div className='flex items-row justify-center gap-x-10 my-14 mx-16'>
+          <div >
+            <img src='https://www.enkiticaret.com.tr/1/images/-fbde1734667a4156a565e9655305cae8.png' alt='' className='w-80 h-80 hover:-translate-y-1 transition duration-500 rounded-t-2xl' />
+          </div>
+          <div>
+            <img src='https://www.enkiticaret.com.tr/1/images/-5984f91b3b6f4aeba3ddff6023ccc5ca.jpg' alt="" className='w-80 h-80 hover:-translate-y-1 transition duration-500 rounded-t-2xl' />
+          </div>
+          <div>
+            <img src='https://www.enkiticaret.com.tr/1/images/-cc2b3c6816944ef4b59a8132feb3d15b.png' alt='' className='w-80 h-80 hover:-translate-y-1 transition duration-500 rounded-t-2xl' />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
